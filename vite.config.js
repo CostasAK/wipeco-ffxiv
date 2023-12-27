7f5b03d9bee1c1b357f453f6bb5e70a075617fbf
CostasAK <costas@kokke.eu>
2023-12-27 18:38:32 +0100

    feat(fonts): add Fira Sans and Fira Code
    Downloaded from Google Fonts by vite-plugin-webfont-dv. Only weights 800 and 900 for Fira Sans and
    400 for Fira Code.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import svgrPlugin from "vite-plugin-svgr";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./env",
-  plugins: [react(), svgrPlugin(), ViteImageOptimizer({ test: /\.svg$/i })],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    ViteImageOptimizer({ test: /\.svg$/i }),
+    webfontDownload(),
+  ],
   test: { environment: "happy-dom" },
 });
