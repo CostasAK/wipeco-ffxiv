7fe4c89bff179d4f6603459f9f16d46182b5bce9
CostasAK <costas@kokke.eu>
2023-12-28 23:29:09 +0100

    style: style using prettier

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -11,7 +11,6 @@ afterEach(() => {
 const customRender = (ui, options = {}) =>
   render(ui, { wrapper: RenderWrapper, ...options });
 
- 
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
 export { customRender as render };
