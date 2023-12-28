d830a6dac9a80b261eaeb143d9edf4967e18a3af
CostasAK <costas@kokke.eu>
2023-12-28 18:36:35 +0100

    refactor: rename .jsx to .js, since there is no JSX in the file

---
+++ src/utils/test-utils.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/export */
+import { cleanup, render } from "@testing-library/react";
+
+import { afterEach } from "vitest";
+import { RenderWrapper } from "../components/RenderWrapper";
+
+afterEach(() => {
+  cleanup;
+});
+
+const customRender = (ui, options = {}) =>
+  render(ui, { wrapper: RenderWrapper, ...options });
+
+ 
+export * from "@testing-library/react";
+export { default as userEvent } from "@testing-library/user-event";
+export { customRender as render };
