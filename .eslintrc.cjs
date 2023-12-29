0a525fa2bc3e73793b04d09326c61bd0f54e0888
CostasAK <costas@kokke.eu>
2023-12-29 08:26:14 +0100

    feat(eslint): configure testing-library eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:import/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:testing-library/react",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
