a3c214198cdbbe8a4e4d49f1f3df3174d58065d2
CostasAK <costas@kokke.eu>
2023-12-29 09:21:13 +0100

    fix: use favicon url instead of importing from public

--- src/components/LogoTitle.jsx
+++ src/components/LogoTitle.jsx
@@ -1,11 +1,10 @@
 import clsx from "clsx";
-import logoSrc from "../../public/favicon.png";
 
 export const LogoTitle = () => {
   return (
     <h1 className={clsx("text-xl", "px-3", "max-w-fit")}>
       <a href="/" className={clsx("flex", "flex-row", "items-center", "gap-4")}>
-        <img alt="" src={logoSrc} className={clsx("w-10", "h-10")} />
+        <img alt="" src="/favicon.png" className={clsx("w-10", "h-10")} />
         <span>Wipe Co.</span>
       </a>
     </h1>
