5e95932beaddec26efa4eaade0983a670b1c08eb
CostasAK <costas@kokke.eu>
2024-01-02 20:39:11 +0100

    build(pnpm): install ical

--- src/components/FullViewportSpinner.jsx
+++ src/components/FullViewportSpinner.jsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
-import logoSrc from "../../public/favicon.png";
 
 export const FullViewportSpinner = () => {
-  return <img alt="Loading..." src={logoSrc} className={clsx("m-10")} />;
+  return <img alt="Loading..." src="/favicon.png" className={clsx("m-10")} />;
 };
